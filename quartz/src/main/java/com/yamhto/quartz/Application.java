ccb2d118079c43ae9fab43101607e405dc966d0b
yangming <88888888>
2019-12-20 16:46:46 +0800

    quartz

---
+++ quartz/src/main/java/com/yamhto/quartz/Application.java
@@ -0,0 +1,19 @@
+package com.yamhto.quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yamhto
+ * @className: Application.java
+ * @package com.yamhto.quartz
+ * @description:
+ * @date 2019/12/20 15:31
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
